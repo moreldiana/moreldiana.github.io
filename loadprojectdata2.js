efffb732dfd8844acbc479793f52b02a560923b2
moreldiana <dianaamorel@gmail.com>
2019-02-26 19:26:55 -0500

    less data

---
+++ loadprojectdata2.js
@@ -0,0 +1,53 @@
+console.log("Welcome to Data Land!");
+
+
+//2018
+
+d3.csv("201801_hubway_tripdata.csv", function(error, data) {
+    console.log("csv:", data);
+});
+
+d3.csv("201802_hubway_tripdata.csv", function(error, data) {
+    console.log("csv:", data);
+});
+
+d3.csv("201803_hubway_tripdata.csv", function(error, data) {
+    console.log("csv:", data);
+});
+
+d3.csv("201804-hubway-tripdata.csv", function(error, data) {
+    console.log("csv:", data);
+});
+
+d3.csv("201805-bluebikes-tripdata.csv", function(error, data) {
+    console.log("csv:", data);
+});
+
+d3.csv("201806-bluebikes-tripdata.csv", function(error, data) {
+    console.log("csv:", data);
+});
+
+d3.csv("201807-bluebikes-tripdata.csv", function(error, data) {
+    console.log("csv:", data);
+});
+
+d3.csv("201808-bluebikes-tripdata.csv", function(error, data) {
+    console.log("csv:", data);
+});
+
+d3.csv("201809-bluebikes-tripdata.csv", function(error, data) {
+    console.log("csv:", data);
+});
+
+d3.csv("201810-bluebikes-tripdata.csv", function(error, data) {
+    console.log("csv:", data);
+});
+
+d3.csv("201811-bluebikes-tripdata.csv", function(error, data) {
+    console.log("csv:", data);
+});
+
+d3.csv("201812-bluebikes-tripdata.csv", function(error, data) {
+    console.log("csv:", data);
+});
+console.log("END OF LINE");
