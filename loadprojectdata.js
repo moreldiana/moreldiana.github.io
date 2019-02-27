320f81d11fb443876794eb07631ea025704f395c
moreldiana <dianaamorel@gmail.com>
2019-02-26 19:23:15 -0500

    html page with scaffolding

--- loadprojectdata.js
+++ loadprojectdata.js
@@ -2,155 +2,151 @@ console.log("Welcome to Data Land!");
 
 //2015 CSVs
 
-d3.csv("201501-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201501-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201502-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201502-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201503-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201503-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201504-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201504-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201505-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201505-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201506-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201506-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201507-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201507-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201508-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201508-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201509-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201509-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201510-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201510-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201511-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201511-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201512-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201512-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-// 2016 CSVs
-
-
-d3.csv("201601-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
-
-d3.csv("201602-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
-
-d3.csv("201603-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
-
-d3.csv("201604-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
-
-d3.csv("201605-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
-
-d3.csv("201606-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
-
-d3.csv("201607-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
-
-d3.csv("201608-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
-
-d3.csv("201609-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
-
-d3.csv("201610-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
-
-d3.csv("201611-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
-
-d3.csv("201612-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
-
-// 2017
+// // 2016 CSVs
 
 
-d3.csv("201701-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201601-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201702-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201602-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201703-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201603-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201704-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201604-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201705-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201605-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201706-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201606-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201707-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201607-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201708-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201608-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201709-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201609-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201710-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201610-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201711-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201611-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
 
-d3.csv("201712-hubway-tripdata.csv", function(error, data) {
-    console.log("csv:", data);
-});
+// d3.csv("201612-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
+
+// // 2017
+
+
+// d3.csv("201701-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
+
+// d3.csv("201702-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
+
+// d3.csv("201703-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
+
+// d3.csv("201704-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
+
+// d3.csv("201705-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
+
+// d3.csv("201706-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
+
+// d3.csv("201707-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
+
+// d3.csv("201708-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
+
+// d3.csv("201709-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
+
+// d3.csv("201710-hubway-tripdata.csv", function(error, data) {
+//     console.log("csv:", data);
+// });
+
+//d3.csv("201712-hubway-tripdata.csv", function(error, data) {
+//    console.log("csv:", data);
+//});
 
 //2018
 
